42847520e1412735a215552398976566ec76fad8
ottodanp <me@ottodanp.dev>
2024-12-07 20:02:19 +0000

    started implementation of http coordinator server requests

--- client.py
+++ client.py
@@ -1,42 +1,63 @@
 import asyncio
-from typing import Optional, Tuple
+from typing import List, Optional, Dict
+from aiohttp import ClientSession
 
-from crypto import generate_keypair
+from crypto import generate_keypair, save_keys, load_keys, keys_exist
+
+
+class RemoteClient:
+    _nickname: str
+    _host: str
+    _port: int
+
+    def __init__(self, nickname: str, host: str, port: int):
+        self._nickname = nickname
+        self._host = host
+        self._port = port
+
+    @property
+    def nickname(self) -> str:
+        return self._nickname
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @property
+    def port(self) -> int:
+        return self._port
 
 
 class PGPChat:
-    _public_key: Optional[str]
-    _private_key: Optional[str]
-    _username: Optional[str]
+    _public_key: str
+    _private_key: str
+    _connected_clients: List[RemoteClient]
+    _coordinator_address: str = "127.0.0.1"
+    _session: ClientSession
 
-    _coordinator_address: Tuple[str, int]
-    _coordinator_writer: asyncio.StreamWriter
-    _coordinator_reader: asyncio.StreamReader
+    def __init__(self, local_nickname: str, session: ClientSession):
+        if not keys_exist(local_nickname):
+            self._public_key, self._private_key = generate_keypair()
+            save_keys(
+                self._public_key,
+                self._private_key,
+                local_nickname
+            )
+            self._session = session
 
-    def __init__(self, coordinator_address: Tuple[str, int]):
-        self._coordinator_address = coordinator_address
+        else:
+            self._public_key, self._private_key = load_keys(local_nickname)
 
-    async def connect(self):
-        self._coordinator_reader, self._coordinator_writer = await asyncio.open_connection(*self._coordinator_address)
+    async def join_swarm(self):
+        async with self._session.get(self.format_coordinator_url("join", args={})) as resp:
+            body = await resp.text()
 
-    async def register(self, username: str):
-        self._username = username
-        self._public_key, self._private_key = generate_keypair()
-
-        self._coordinator_writer.write(f"register::{username}::{self._public_key}\n".encode('utf8'))
-        response = await self._coordinator_reader.read(100)
-
-        if response != b"Registered\n":
-            print("Registration failed")
-            return
-
-        print("Registration successful")
+    def format_coordinator_url(self, endpoint: str, args: Optional[Dict[str, str]]) -> str:
+        return f"http://{self._coordinator_address}/{endpoint}"
 
 
 async def main():
-    chat = PGPChat(("localhost", 15555))
-    await chat.connect()
-    await chat.register("Alice")
+    chat = PGPChat("Otto")
 
 
 if __name__ == "__main__":
--- crypto.py
+++ crypto.py
@@ -1,3 +1,7 @@
+from os import makedirs
+from os.path import exists
+from typing import Tuple, Optional
+
 import pgpy
 
 
@@ -22,10 +26,33 @@ def decrypt_message(encrypted_message: str, private_key: str) -> str:
         return f"Error during decryption: {e}"
 
 
-def generate_keypair() -> (str, str):
+def generate_keypair() -> Tuple[str, str]:
     key = pgpy.PGPKey.new(pgpy.constants.PubKeyAlgorithm.RSAEncryptOrSign, 4096)
 
     public_key = str(key.pubkey)
     private_key = str(key)
 
     return public_key, private_key
+
+
+def keys_exist(name: str) -> bool:
+    return exists(f"data/keychain/{name}.pri") and exists(f"data/keychain/{name}.pub")
+
+
+def save_keys(public_key: str, private_key: str, name: str) -> None:
+    if not exists("data/keychain"):
+        makedirs("data/keychain")
+
+    with open(f"data/keychain/{name}.pub", "w") as pkey_file:
+        pkey_file.write(public_key)
+
+    with open(f"data/keychain/{name}.pri", "w") as privkey_file:
+        privkey_file.write(private_key)
+
+
+def load_keys(name: str) -> Optional[Tuple[str, str]]:
+    if keys_exist(name):
+        with open(f"data/keychain/{name}.pri", "r") as privkey_file:
+            with open(f"data/keychain/{name}.pub", "r") as pubkey_file:
+                return pubkey_file.read(), privkey_file.read()
+
